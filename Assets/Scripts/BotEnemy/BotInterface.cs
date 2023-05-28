f355713570658553db25f67218f440a483257954
RUDIK <vladislavrudakovskij@gmail.com>
2023-05-28 13:00:49 +0300

    enemy AI
    -patrol

--- Assets/Scripts/BotEnemy/BotInterface.cs
+++ Assets/Scripts/BotEnemy/BotInterface.cs
@@ -1,15 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class BotInterface : MonoBehaviour
 {
+    //AI
+    [Header("AI")]
+    [SerializeField] private NavMeshAgent _agent;
     //for movement
     [Header("For Movement")]
     [SerializeField] public float distanceToAttack;
     [SerializeField] public float distanceToSee;
     [SerializeField] private float _speed;
     [SerializeField] private float _turnSpeed;
+    [SerializeField] private float _distanceForPatrol;
     //for entaraction
     [Header("For Fight")]
     [SerializeField] private int _hp;
@@ -18,21 +23,25 @@ public class BotInterface : MonoBehaviour
 
     public bool canFire = true;
     [SerializeField]private LineRenderer _lineRender;
+    //public bool _havePoint;
+    public bool isPatrol;
 
     private void Start()
     {
+        _agent = GetComponent<NavMeshAgent>();
         _lineRender = GetComponent<LineRenderer>();
     }
 
-    public void GoToTarget(Transform target)
+    public void GoToTarget(Vector3 target)
     {
-        transform.position = Vector3.MoveTowards(transform.position, target.position, _speed);
+        //transform.position = Vector3.MoveTowards(transform.position, target, _speed);
         RotateToTarget(target);
+        _agent.Move(transform.TransformDirection(Vector3.forward * _speed));
     }
 
-    public void RotateToTarget(Transform target)
+    public void RotateToTarget(Vector3 target)
     {
-        var targetRotation = Quaternion.LookRotation(new Vector3(target.position.x, target.position.y, target.position.z) - transform.position, Vector3.up);
+        var targetRotation = Quaternion.LookRotation(new Vector3(target.x, target.y, target.z) - transform.position, Vector3.up);
         transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, _turnSpeed);
     }
 
@@ -50,6 +59,24 @@ public class BotInterface : MonoBehaviour
         Destroy(gameObject);
     }
 
+    public Vector3 NewDestination()
+    {
+        //create new point
+        return new Vector3(transform.position.x + Random.Range(-_distanceForPatrol, _distanceForPatrol), 0, transform.position.z + Random.Range(-_distanceForPatrol, _distanceForPatrol));
+    }
+
+    public bool CheckDistance(Vector3 destination)
+    {
+        return Vector3.Distance(transform.position, destination) < 0.5f;
+    }
+
+    public void GoToPatrolPoint(Vector3 destination)
+    {
+        // go to point
+        RotateToTarget(destination);
+        _agent.SetDestination(destination);
+    }
+
     public void Attack()
     {
         RaycastHit hit;
@@ -59,10 +86,12 @@ public class BotInterface : MonoBehaviour
             {
                // hit.collider.GetComponent<AllyInterface>().GetDamage(_damage);
                 StartCoroutine(Reloading());
+                //laser
                 _lineRender.SetPosition(0, transform.position);
                 _lineRender.SetPosition(1, hit.collider.transform.position);
             }
             Debug.Log(hit);
+            
         }
         
     }
--- Assets/Scripts/BotEnemy/BotMovement.cs
+++ Assets/Scripts/BotEnemy/BotMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class BotMovement : BotInterface
 {
     [SerializeField] private Transform _player;
+    public Vector3 des;
     void Start()
     {
         
@@ -14,21 +15,40 @@ public class BotMovement : BotInterface
     void Update()
     {
         //Debug.Log(Vector3.Distance(transform.position, _player.position));
-        if (distanceToSee > Vector3.Distance(transform.position, _player.position) && distanceToAttack < Vector3.Distance(transform.position, _player.position))
+        if (distanceToSee >= Vector3.Distance(transform.position, _player.position) && distanceToAttack < Vector3.Distance(transform.position, _player.position))
         {
-            GoToTarget(_player);
+            GoToTarget(_player.position);
             Debug.Log("gotoplayer");
+            isPatrol = false;
         }
-        else if (distanceToAttack > Vector3.Distance(transform.position, _player.position) && canFire)
+        else if (distanceToAttack >= Vector3.Distance(transform.position, _player.position) && canFire)
         {
             //attack
             Attack();
             Debug.Log("attack");
         }
-        else
+        else if (distanceToSee < Vector3.Distance(transform.position, _player.position))
         {
+            Debug.Log("Patrol");
+           //when enemy not near go to point
+            if (!isPatrol)
+            {
+                //create new point and set destination
+                isPatrol = true;
+                des = NewDestination();
+                GoToPatrolPoint(des);
+            }
+
+            //if enemy near the point he is stop
+            if (CheckDistance(des))
+            {
+                isPatrol = false;
+            }
             //patrol
-            Debug.Log("patrol");
         }
+        {
+           
+        }
+        
     }
 }
